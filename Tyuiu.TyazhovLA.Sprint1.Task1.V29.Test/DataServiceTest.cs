24a1897477ed4d6dac122c6c428e081a6b89861d
Tyuiu.TyazhovLA@outlook.com <99raindeath@gmail.com>
2024-09-15 13:25:00 +0500

    Task1

---
+++ Tyuiu.TyazhovLA.Sprint1.Task1.V29.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.TyazhovLA.Sprint1.Task1.V29.Lib;
+namespace Tyuiu.TyazhovLA.Sprint1.Task1.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Valid228()
+        {
+            DataService ds = new DataService();
+            double a = 1.0;
+            double b = 2.0;
+            double c = 3.0;
+            var res = ds.Calculate(a, b, c);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
---
+++ Tyuiu.TyazhovLA.Sprint1.Task1.V29/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.TyazhovLA.Sprint1.Task1.V29.Lib;
+namespace Tyuiu.TyazhovLA.Sprint1.Task1.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Тяжов Л. А. | ПКТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Организация ввода/вывода в консольных приложениях                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Тяжов Леонид Александрович | ПКТб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле a*b*c/(a+b+c) и печатает его на экране.  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double a, b, c;
+            Console.WriteLine("Введите значение A:");
+            a= Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение B:");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение C:");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(a, b, c));
+            Console.ReadKey();
+
+        }
+    }
+}
